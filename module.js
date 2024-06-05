ef491645d2a16e79413468b0cc19ce236724bfb2
OrochiDrake <andrezaguette@gmail.com>
2024-06-05 12:51:51 -0300

    Update system configuration and module for Foundry VTT 11.35 compatibility

--- module.js
+++ module.js
@@ -1,9 +1,100 @@
+class SkateAndWizardsActor extends Actor {
+  prepareData() {
+    super.prepareData();
+    const actorData = this.data;
+    // Initialize attribute data if not present
+    if (!actorData.data.attributes) {
+      actorData.data.attributes = {
+        strength: 0,
+        dexterity: 0,
+        will: 0
+      };
+    }
+    if (!actorData.data.spells) {
+      actorData.data.spells = {
+        permanent: ["", "", ""],
+        random: ["", "", ""],
+        bootleg: ""
+      };
+    }
+    if (!actorData.data.items) {
+      actorData.data.items = ["", "", "", ""];
+    }
+  }
+
+  rollRandomSpell(target) {
+    const firstColumn = [
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_1"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_2"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_3"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_4"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_5"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_6"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_7"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_8"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_9"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_10"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_11"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_1_12")
+    ];
+    const secondColumn = [
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_1"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_2"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_3"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_4"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_5"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_6"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_7"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_8"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_9"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_10"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_11"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_2_12")
+    ];
+    const thirdColumn = [
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_1"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_2"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_3"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_4"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_5"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_6"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_7"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_8"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_9"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_10"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_11"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_3_12")
+    ];
+    const fourthColumn = [
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_1"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_2"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_3"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_4"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_5"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_6"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_7"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_8"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_9"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_10"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_11"),
+      game.i18n.localize("SKATE_AND_WIZARDS.SPELL_PART_4_12")
+    ];
+
+    function rollD12() {
+      return Math.floor(Math.random() * 12);
+    }
+
+    const spell = `${firstColumn[rollD12()]} ${secondColumn[rollD12()]} ${thirdColumn[rollD12()]} ${fourthColumn[rollD12()]}`;
+    this.update({[`data.${target}`]: spell});
+  }
+}
+
 Hooks.once('init', async function() {
   console.log('Skate and Wizards | Initializing Skate and Wizards System');
 
   // Define custom Entity classes
-  CONFIG.Actor.entityClass = SkateAndWizardsActor;
-  CONFIG.Item.entityClass = SkateAndWizardsItem;
+  CONFIG.Actor.documentClass = SkateAndWizardsActor;
+  CONFIG.Item.documentClass = SkateAndWizardsItem;
 
   // Preload Handlebars templates
   await preloadHandlebarsTemplates();
@@ -72,29 +163,4 @@ async function preloadHandlebarsTemplates() {
   ];
   
   return loadTemplates(templatePaths);
-}
-
-class SkateAndWizardsActor extends Actor {
-  prepareData() {
-    super.prepareData();
-    const actorData = this.data;
-    // Initialize attribute data if not present
-    if (!actorData.data.attributes) {
-      actorData.data.attributes = {
-        strength: 0,
-        dexterity: 0,
-        will: 0
-      };
-    }
-    if (!actorData.data.spells) {
-      actorData.data.spells = {
-        permanent: ["", "", ""],
-        random: ["", "", ""],
-        bootleg: ""
-      };
-    }
-    if (!actorData.data.items) {
-      actorData.data.items = ["", "", "", ""];
-    }
-  }
 }
